0272beea41a8c17c0c376e18262686f2aefc03db
Riday ♡ <ridayshah2001@gmail.com>
2022-03-20 17:12:44 +0530

    🚩 Chore: added more emission factors
    Co-authored-by: Shivani Jha <shivaniijha99@gmail.com>

--- api/estimate/emissionFactors.js
+++ api/estimate/emissionFactors.js
@@ -5,6 +5,18 @@ const emissionFactors = [
 	'consumer_goods-type_food_beverages_tobacco',
 	'electrical_equipment-type_electrical_optical_equipment',
 	'fuel_type_coke_refined_petroleum_nuclear_fuel-fuel_use_na',
-	'agriculture_fishing_forestry-type_agriculture_hunting_forestry_fishing'
+	'agriculture_fishing_forestry-type_agriculture_hunting_forestry_fishing',
+	'accommodation_type_hotel_room',
+	'chemicals-type_other_non_metallic_mineral',
+	'consumer_goods-type_leather_footwear',
+	'consumer_services-type_sale_maintenance_repair_of_motor_vehicles_motorcycles_retail_sale_of_fuel',
+	'education-type_na',
+	'equipment_rental-type_renting_of_machinery_equipment_other_business_activities',
+	'freight_flight-route_type_na-distance_na-weight_na-rf_na',
+	'freight_train-route_type_na-fuel_type_na',
+	'freight_vehicle-vehicle_type_na-fuel_source_na-vehicle_weight_na-percentage_load_na',
+	'health_social_care-type_health_social_work',
+	'paper_products-type_pulp_paper_printing_publishing',
+	'passenger_ferry-route_type_na-fuel_source_na'
 ];
 module.exports = emissionFactors;
